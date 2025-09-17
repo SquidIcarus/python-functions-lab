9ead9a8c2e4aae9799e645eea01e2fd2f618cf99
User Name <landonisaiah@gmail.com>
2025-09-17 13:55:36 -0700

    exercise 3 complete

---
+++ exercise03.py
@@ -0,0 +1,19 @@
+# Exercise 3: Apply a Discount
+#
+# Write a function named `apply_discount` that takes a product's price and a discount percentage (from 0 to 100).
+# Return the new price after applying the discount.
+#
+# Examples:
+# apply_discount(100, 25) should return 75.
+# apply_discount(80, 10) should return 72.
+#
+# Define your function and call it to display the discounted price.
+
+def apply_discount(price, perc):
+    dec_string = "0." + str(perc)
+    dec_numb = float(dec_string)
+    return (price * (1 - dec_numb))
+
+print('Exercise 3:', apply_discount(100, 25))
+print(apply_discount(80, 10))
+print(apply_discount(59.99, 50))
